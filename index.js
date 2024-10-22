f58ce97489be9d15a3693002e797f0d3996440b3
Glitch (whimsical-honey-morocco) <none>
2024-10-22 17:24:32 +0000

    glitch

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
+const http = require("http");
+const httpProxy = require("http-proxy");
 
 // Создаем прокси-сервер
 const proxy = httpProxy.createProxyServer({});
 
 // Создаем сервер
 const server = http.createServer((req, res) => {
-  const targetUrl = 'http://example.com'; // Указываем целевой сервер
+  const targetUrl = "http://example.com"; // Указываем целевой сервер
 
   proxy.web(req, res, { target: targetUrl }, (err) => {
-    console.error('Proxy error:', err);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Something went wrong.');
+    console.error("Proxy error:", err);
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Something went wrong.");
   });
 });
 
 server.listen(3000, () => {
-  console.log('Proxy server is running on port 3000');
+  console.log("Proxy server is running on port 3000");
 });
